3b196132a7d5a87a1fec29f4509cbde398052a4b
Krzysztof Madejski <krzysztof@madejscy.pl>
2018-05-30 09:14:41 -0500

    Tweaks on research page

--- moduls/section-research.php
+++ moduls/section-research.php
@@ -6,50 +6,46 @@
     </h2>
     
     <div class="slide-lead">We looked at the various impacts of civic tech while conducting this research. Our main aim is <span class="inverse">community-wide peer learning.</span></div>
-    
+
 
     <div class="research-columns">
 
       <div class="reserach-column">
           <div class="research-link">
-            <h5><a href="https://transparencee.org/news/civic-meets-tech-get-along/">When civic meets tech: How do they
-get along with each other?</a></h5>
-            <!-- <a href="" class="additional-link">Watch video</a> -->
+              <h5><a href="https://transparencee.org/news/civic-meets-tech-get-along/">When civic meets tech: How do
+                      they
+                      get along with each other?</a></h5>
+              <!-- <a href="" class="additional-link">Watch video</a> -->
           </div>
 
           <div class="research-link">
-            <h5><a href="http://peoplebeforetech.transparencee.org/">How to measure success of civic-tech
-projects? People before Technology</a></h5>
-
+              <h5><a href="http://peoplebeforetech.transparencee.org/">How to measure success of civic-tech
+                      projects? People before Technology</a></h5>
           </div>
 
-<div class="research-link">
-
-            <h5><a href="http://techfails.transparencee.org/">Civic tech tools that did not meet expectations: Lessons learned from the field</a></h5>
-            
+          <div class="research-link">
+              <h5><a href="http://techfails.transparencee.org/">Civic tech tools that did not meet expectations: Lessons
+                      learned from the field</a></h5>
           </div>
-
-          
-
       </div>
 
       <div class="reserach-column">
 
           <div class="research-link">
-            <h5><a href="https://www.youtube.com/watch?v=Gz5tf1u3kts">Watch: When civic meets tech: How do they get along with each other?</a></h5>
+              <h5><a href="https://www.youtube.com/watch?v=Gz5tf1u3kts"><span
+                              style="font-weight: 800;">Watch video</span>: When civic meets tech</a></h5>
           </div>
-
 
           <div class="research-link">
-
-            <h5><a href="http://opendata-goodpractices.transparencee.org/">Open-data project tailored to the audience needs: Good practices and lessons learned</a></h5>
-            
+              <h5><a href="http://techforjournalists.transparencee.org/">Technology for Journalists: Peer Advice from
+                      Regional Journalistic NGOs</a></h5>
           </div>
 
-          
+          <div class="research-link">
+              <h5><a href="http://opendata-goodpractices.transparencee.org/">Open-data project tailored to the audience
+                      needs: Good practices and lessons learned</a></h5>
+          </div>
 
-              
-        
       </div>
     </div>
 
